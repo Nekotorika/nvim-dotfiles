086e11d4569aaec1ac3656bbc5950bf3e3a53d5a
Nekotorika <nakagawahauhausu@gmail.com>
2026-04-20 01:47:29 +0900

    fixes

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -14,12 +14,10 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = {
       "echasnovski/mini.icons",
-      "catppuccin/nvim",
     },
     config = function()
       require("lualine").setup({
         options = {
-          theme = "catppuccin",
           icons_enabled = true,
         },
         sections = {
