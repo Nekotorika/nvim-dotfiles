99e152036cb3fa00f69514de491879c5c7cea962
Nekotorika <nakagawahauhausu@gmail.com>
2026-07-24 11:50:18 +0900

    修正

--- lua/plugins/preview.lua
+++ lua/plugins/preview.lua
@@ -1,9 +1,13 @@
 return {
   "iamcco/markdown-preview.nvim",
+  priority = 1000,
+  lazy = false,
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
   build = function()
     vim.fn["mkdp#util#install"]()
+  end,
+  config = function()
     vim.keymap.set(
       "n",
       "<leader>m",
